bd9abe73fb7181fd689f64737a6a29572077d2e4
Arthur Pastel <arthur.pastel@gmail.com>
2019-10-06 10:38:42 +0200

    Dirty files fix

--- main.c
+++ main.c
@@ -5,15 +5,17 @@ void secretFunction()
     printf("Congratulations!\n");
     printf("You have entered in the secret function!\n");
 }
+
 void echo()
 {
     char buffer[20];
-    while(1){
-      printf("Enter some text:\n");
-      fflush(stdout);
-      scanf("%s", buffer);
-      printf("You entered: %s\n", buffer);
-      fflush(stdout);
+    while (1)
+    {
+        printf("Enter some text:\n");
+        fflush(stdout);
+        scanf("%s", buffer);
+        printf("You entered: %s\n", buffer);
+        fflush(stdout);
     }
 }
 
